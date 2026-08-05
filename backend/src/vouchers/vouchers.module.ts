4978b629be19438e434b6f67166230e01549e157
thongdym <thong-l@dymvietnam.net>
2026-08-05 15:15:37 +0700

    fix voucher

--- backend/src/vouchers/vouchers.module.ts
+++ backend/src/vouchers/vouchers.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { VouchersService } from './vouchers.service';
+import { VouchersController } from './vouchers.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
+  controllers: [VouchersController],
   providers: [VouchersService],
   exports: [VouchersService]
 })
